90d7c2d8d45bba783067b59430f34a63ecc98f79
Diego <145839619+AKAMois@users.noreply.github.com>
2025-05-13 16:40:19 +0200

    Aactualizacion: pequeño cambio en el codigo

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const mongoose = require('mongoose'); // Importamos mongoose para conectarnos a MongoDB
-const app = require('./app'); // Importamos la aplicación principal
-const port = 3000; // Añadimos el puerto en una variable
-const urlMongoAtlas = "mongodb+srv://god:godgodgod@taskdb.p9glwvt.mongodb.net/?retryWrites=true&w=majority&appName=taskdb"; // URL de conexión a MongoDB Atlas
+const mongoose = require("mongoose"); // Importamos mongoose para conectarnos a MongoDB
+const app = require("./app"); // Importamos la aplicación principal
+const port = process.env.PORT || 3979; // Añadimos el puerto en una variable
+const urlMongoAtlas =
+  "mongodb+srv://god:godgodgod@taskdb.p9glwvt.mongodb.net/?retryWrites=true&w=majority&appName=taskdb"; // URL de conexión a MongoDB Atlas
 
 // Creamos una función async para manejar la conexión a la base de datos y levantar el servidor
 async function startServer() {
   try {
     // Intentamos conectarnos a MongoDB
     await mongoose.connect(urlMongoAtlas);
-    console.log('La conexión a la base de datos es correcta'); // Si la conexión es correcta, mostramos este mensaje
+    console.log("La conexión a la base de datos es correcta"); // Si la conexión es correcta, mostramos este mensaje
 
     // Si la conexión fue exitosa, levantamos el servidor
     app.listen(port, () => {
-      console.log('Servidor levantado'); // Confirmamos que el servidor está corriendo
+      console.log("Servidor levantado en http://localhost:" + port); // Confirmamos que el servidor está corriendo
     });
-
   } catch (error) {
     // En caso de error en la conexión, lo mostramos en consola
-    console.error('Error al conectar a la base de datos:', error);
+    console.error("Error al conectar a la base de datos:", error);
   }
 }
 
